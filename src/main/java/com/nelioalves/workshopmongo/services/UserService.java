e0de3fb56af07542c0505706d4dc736dc66cf9b6
renatof4rias <renatof4rias@gmail.com>
2023-07-05 12:01:01 -0300

    Commit 06 - Tratamento de Excecoes Personalizadas

--- src/main/java/com/nelioalves/workshopmongo/services/UserService.java
+++ src/main/java/com/nelioalves/workshopmongo/services/UserService.java
@@ -1,21 +1,30 @@
 package com.nelioalves.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nelioalves.workshopmongo.domain.User;
 import com.nelioalves.workshopmongo.repository.UserRepository;
+import com.nelioalves.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository repo; 
+
 	
 	public List<User> findAll(){
 		return repo.findAll();
 	}
+	
+	public User findById(String id){
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado"));
+	}
+	
 	
 }
